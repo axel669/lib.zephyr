a7a4df10fa5227211f61a6be84dbe629f78dbd33
Axel669 <haryuu.hanekata@gmail.com>
2023-04-13 21:56:23 -0700

    add adorn to text

--- src/text.svelte
+++ src/text.svelte
@@ -4,10 +4,12 @@
     export let title = false
     export let subtitle = false
     export let block = false
+    export let adorn = false
 
     $: wind = {
         "$title-text": title,
         "$subtitle": subtitle,
+        "$adorn": adorn,
         block,
         ...$$restProps
     }
