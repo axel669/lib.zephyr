bbc850161c62fc23834d4b56d20f87ca313adacf
Axel669 <haryuu.hanekata@gmail.com>
2023-04-27 16:42:44 -0700

    alert dialog: added

---
+++ src/dialogs/alert.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Button from "../button.svelte"
+    import Dialog from "../dialog.svelte"
+    import Icon from "../icon.svelte"
+    import Text from "../text.svelte"
+    import Titlebar from "../titlebar.svelte"
+
+    export let close
+    export let title = "Alert"
+    export let icon
+    export let message
+    export let okText = "OK"
+    export let color = "primary"
+</script>
+
+<Dialog card {color}>
+    <Titlebar slot="header" {color}>
+        <Text title slot="title">
+            <Icon name={icon}>
+                {title}
+            </Icon>
+        </Text>
+    </Titlebar>
+
+    <Text>
+        {message}
+    </Text>
+
+    <Button slot="footer" on:click={close} {color}>
+        {okText}
+    </Button>
+</Dialog>
