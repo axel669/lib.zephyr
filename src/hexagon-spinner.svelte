382996b6524bc93fa70bdf881484f6e0678f24b4
Axel669 <haryuu.hanekata@gmail.com>
2023-04-27 16:43:28 -0700

    hexagon spinner: added

---
+++ src/hexagon-spinner.svelte
@@ -0,0 +1,13 @@
+<script>
+    import wsx from "./wsx.mjs"
+
+    export let size = "100px"
+    export let color = "primary"
+
+    $: wind = {
+        "&size": size,
+        $color: color,
+    }
+</script>
+
+<ws-hexagon-spinner use:wsx={wind} />
