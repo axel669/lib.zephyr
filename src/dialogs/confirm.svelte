e80503a0cba257da5997d8081bd67d3d8df8937d
Axel669 <haryuu.hanekata@gmail.com>
2023-04-27 16:42:52 -0700

    confirm dialog: added

---
+++ src/dialogs/confirm.svelte
@@ -0,0 +1,43 @@
+<script>
+    import Button from "../button.svelte"
+    import Dialog from "../dialog.svelte"
+    import Grid from "../grid.svelte"
+    import Icon from "../icon.svelte"
+    import Text from "../text.svelte"
+    import Titlebar from "../titlebar.svelte"
+
+    import { handler$ } from "../handler$.mjs"
+
+    export let close
+    export let title = "Confirm"
+    export let icon
+    export let message
+    export let okText = "OK"
+    export let cancelText = "Cancel"
+    export let color = "primary"
+
+    const cls = handler$(close)
+</script>
+
+<Dialog card {color}>
+    <Titlebar slot="header" {color}>
+        <Text title slot="title">
+            <Icon name={icon}>
+                {title}
+            </Icon>
+        </Text>
+    </Titlebar>
+
+    <Text>
+        {message}
+    </Text>
+
+    <Grid cols="1fr 1fr" slot="footer" pad="0px">
+        <Button on:click={cls(false)} color="danger">
+            {cancelText}
+        </Button>
+        <Button on:click={cls(true)} color="secondary">
+            {okText}
+        </Button>
+    </Grid>
+</Dialog>
