33eb77deb9cd182354de0994c1e03b9e9854111d
Axel669 <haryuu.hanekata@gmail.com>
2023-04-27 16:43:15 -0700

    circle spinner: added

---
+++ src/circle-spinner.svelte
@@ -0,0 +1,13 @@
+<script>
+    import wsx from "./wsx.mjs"
+
+    export let size = "100px"
+    export let color = "primary"
+
+    $: wind = {
+        "&size": size,
+        $color: color,
+    }
+</script>
+
+<ws-circle-spinner use:wsx={wind} />
