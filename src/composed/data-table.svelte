980ba54b04c34d3ccc8e5552e709a9455d85a826
axel669 <haryuu.hanekata@gmail.com>
2023-12-02 00:55:49 -0800

    [change] rewrite data table to leverage table and use slot customization

--- src/composed/data-table.svelte
+++ src/composed/data-table.svelte
@@ -2,6 +2,7 @@
     import Button from "../control/button.svelte"
     import Icon from "../info/icon.svelte"
     import Paper from "../layout/paper.svelte"
+    import Table from "../data-display/table.svelte"
     import Text from "../text.svelte"
 
     import Grid from "../layout/grid.svelte"
@@ -9,66 +10,57 @@
     import wsx from "../wsx.mjs"
 
     export let color = false
+    export let fillHeader = true
     export let data = []
-    export let cols = []
-    export let rowWSX = null
-    export let cellWSX = null
     export let pageSize = 10
     export let page = 0
     export let rowHeight = "40px"
-
-    $: wind = {
-        "$color": color,
-        ...$$restProps,
-    }
 
     $: rows = Array.from(
         { length: pageSize },
         (_, i) => data[page * pageSize + i]
     )
-    $: pageCount = Math.ceil(data.length / pageSize)
+    $: rowCount = data?.length ?? 0
+    $: pageCount = Math.ceil(rowCount / pageSize)
+    $: maxPage = Math.max(pageCount - 1, 0)
 
     const prev = () => page = Math.max(0, page - 1)
-    const next = () => page = Math.min(pageCount - 1, page + 1)
+    const next = () => page = Math.min(maxPage, page + 1)
 </script>
 
 <Paper card {color} l-fl.cross="stretch" l-p="0px">
-    <table use:wsx={wind}>
-        <thead>
-            <tr>
-                {#each cols as col}
-                    <th use:wsx={{ w: col.width, h: rowHeight }}>{col.label}</th>
-                {/each}
+    <Table data={rows} {color} {fillHeader} {...$$restProps}>
+        <slot slot="header" let:colNames>
+            <tr ws-x="h[{rowHeight}]">
+                <slot name="header">
+                    <th>No Header Template</th>
+                </slot>
             </tr>
-        </thead>
-        <tbody>
-            {#each rows as row, rowNum}
-                {#if row === undefined}
-                    <tr use:wsx={{ h: rowHeight }}></tr>
-                {:else}
-                    <tr use:wsx={{ h: rowHeight, ...rowWSX?.(row, rowNum) }}>
-                        {#each cols as col, colNum}
-                            <td use:wsx={cellWSX?.(row, rowNum, col)}>
-                                {(col.format !== undefined)
-                                    ? col.format?.(row)
-                                    : row[col.prop]
-                                }
-                            </td>
-                        {/each}
-                    </tr>
-                {/if}
-            {/each}
-        </tbody>
-    </table>
-    <Grid slot="footer" gr-col="min-content min-content min-content 1fr">
-        <Button on:click={prev}>
+        </slot>
+        <slot slot="row" let:row let:colNames>
+            <tr ws-x="h[{rowHeight}]">
+                <slot name="row" {row}>
+                    <td>No Row Template</td>
+                </slot>
+            </tr>
+        </slot>
+        <slot slot="empty-row">
+            <tr ws-x="h[{rowHeight}]">
+                <slot name="empty-row" />
+            </tr>
+        </slot>
+    </Table>
+    <Grid slot="footer" gr.cols="min-content min-content min-content 1fr">
+        <Button on:click={prev} disabled={page === 0}>
             <Icon name="arrow-big-left" />
         </Button>
-        <Button on:click={next}>
+        <Button on:click={next} disabled={page === maxPage}>
             <Icon name="arrow-big-right" />
         </Button>
-        <Text adorn t-ws="nowrap">
-            Page {page + 1} / {pageCount}
+        <Text adorn t.ws="nowrap">
+            {#if pageCount !== 0}
+                Page {page + 1} / {pageCount}
+            {/if}
         </Text>
     </Grid>
 </Paper>
