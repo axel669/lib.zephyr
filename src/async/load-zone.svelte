b0fec58bbc1d58dded3060128cc3a1f85b9690e0
axel669 <haryuu.hanekata@gmail.com>
2023-12-14 18:29:03 -0800

    [add/load-zone] message prop for quick customization when loading

--- src/async/load-zone.svelte
+++ src/async/load-zone.svelte
@@ -1,13 +1,14 @@
 <script>
     import Spinner from "../spinner/hexagon-spinner.svelte"
     export let source = null
+    export let message = "Loading"
 </script>
 
 {#if source !== null}
     {#await source}
         <slot name="loading">
             <ws-flex ws-x="fl.cross[center] fl.main[center]">
-                <span>Loading</span>
+                <span>{message}</span>
                 <Spinner size="100px" />
             </ws-flex>
         </slot>
