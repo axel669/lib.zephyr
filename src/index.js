3be7fb877ed12eeae115a91b25af418b109c643f
axel669 <haryuu.hanekata@gmail.com>
2025-04-13 01:35:45 -0700

    [update] keep exports in line with current components

--- src/index.js
+++ src/index.js
@@ -39,13 +39,12 @@ export { default as HexagonSpinner } from "./spinner/hexagon-spinner.svelte"
 export { default as Alert } from "./dialogs/alert.svelte"
 export { default as Confirm } from "./dialogs/confirm.svelte"
 
-// export { default as DataTable } from "./composed/data-table.svelte"
+export { default as DataTable } from "./composed/data-table.svelte"
 // export { default as TH } from "./composed/data-table/th.svelte"
 export { default as EntryButton } from "./composed/entry-button.svelte"
 // export { default as Form } from "./composed/form.svelte"
 
 export { default as AsyncButton } from "./async/button.svelte"
-// export { default as LoadZone } from "./async/load-zone.svelte"
 export { default as Suspend } from "./async/suspend.svelte"
 
 export { default as Route } from "./routing/route.svelte"
