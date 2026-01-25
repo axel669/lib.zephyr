ebb7bcd0b1716850d0c4d2df70ca13203b876488
axel669 <haryuu.hanekata@gmail.com>
2026-01-24 19:07:38 -0800

    [change] setup docsite differently during dev

--- docsite/app.svelte
+++ docsite/app.svelte
@@ -1,32 +1,86 @@
+<svelte:options runes />
 <script>
     import * as ze from "#lib"
+    // import * as ze from "#lib"
 
-    import Docs from "#comp/docs"
-    import SideMenu from "#comp/side-menu"
-    import { theme } from "#state/theme"
+    // import Docs from "#comp/docs"
+    // import SideMenu from "#comp/side-menu"
+    // import { theme } from "#state/theme"
 
-    import README from "#README"
+    // import README from "#README"
 
-    import examples from "$examples"
+    // import examples from "$examples"
 
-    const page = ze.stackStore("Home")
+    // const page = ze.stackStore("Home")
 
-    const options = [
-        { label: "Theme: Light", value: "light" },
-        { label: "Theme: Dark", value: "dark" },
-        { label: "Theme: Tron", value: "tron" },
-    ]
+    let open = $state(false)
+
+    let theme = $state("tron")
+
+    const test = () => "testing?"
 </script>
 
-<ze.Title format={data => `Zephyr - ${data}`} data="Home" />
+<!-- <ze.Title format={data => `Zephyr - ${data}`} data="Home" /> -->
 
-<svelte:head>
+<!-- <svelte:head>
     <link href="https://cdn.jsdelivr.net/npm/prismjs@v1.29.0/themes/prism-twilight.css" rel="stylesheet" />
     <link href="./md-fix.css" rel="stylesheet" />
 </svelte:head>
-<svelte:body use:ze.wsx={{"@@theme": $theme, "@@app": true}} />
+<svelte:body use:ze.wsx={{"@@theme": $theme, "@@app": true}} /> -->
+<svelte:body use:ze.wsData={`#theme.${theme}; @base-radius: 0px; ! * { outln: none; }`} />
 
-<ze.Screen alignLeft width="100%">
+<ze.Screen ws="@pad-left: 0px;" paperWS="@color: @primary; variant.outline;">
+    {#snippet header()}
+        <ze.Titlebar ws="variant.fill;">
+            <ze.Text title>
+                Zephyr Docs
+            </ze.Text>
+
+            {#snippet action()}
+                <ze.Select ws="@color: @accent;" bind:value={theme}>
+                    <ws-option value="light">
+                        <ze.Icon name="sun" />
+                    </ws-option>
+                    <ws-option value="dark">
+                        <ze.Icon name="moon" />
+                    </ws-option>
+                    <ws-option value="tron">
+                        <ze.Icon name="hexagon-3d" />
+                    </ws-option>
+                </ze.Select>
+            {/snippet}
+
+            {#snippet menu()}
+                <ze.Button onclick={() => open = true}>
+                    <ze.Icon name="menu-2" />
+                </ze.Button>
+            {/snippet}
+        </ze.Titlebar>
+    {/snippet}
+
+    <ze.ControlLabel label="Test">
+        <input type="text" />
+    </ze.ControlLabel>
+    <ze.ControlLabel ws="@color: @info; variant.outline;">
+        <input type="text" />
+    </ze.ControlLabel>
+    <ze.ControlLabel ws="@color: @success; variant.lined;" label="Lined">
+        <input type="text" />
+    </ze.ControlLabel>
+
+    <ze.Drawer ws="w: min(80vw, 240px);">
+        <ze.Paper ws="variant.outline; @color: @info;">
+            {#snippet header()}
+                <div>header</div>
+            {/snippet}
+            <div>some text</div>
+            <div>some text</div>
+        </ze.Paper>
+    </ze.Drawer>
+</ze.Screen>
+
+
+<!-- <ze.Screen alignLeft width="100%">
     <ze.Paper square l!p="0px">
         {#snippet header()}
         <ze.Titlebar fill color="@primary">
@@ -58,4 +112,4 @@
             {/each}
         </ze.Flex>
     </ze.Paper>
-</ze.Screen>
+</ze.Screen> -->
--- docsite/rollup.config.js
+++ docsite/rollup.config.js
@@ -1,5 +1,5 @@
-import fs from "node:fs/promises"
-import path from "node:path"
+// import fs from "node:fs/promises"
+// import path from "node:path"
 
 import asuid from "@axel669/asuid"
 import svelte from "rollup-plugin-svelte"
@@ -9,93 +9,93 @@ import terser from "@rollup/plugin-terser"
 import del from "rollup-plugin-delete"
 import copy from "@axel669/rollup-copy-static"
 
-import { marked } from "marked"
-import Prism from "prismjs"
-import loadLang from "prismjs/components/index.js"
-import "prism-svelte"
+// import { marked } from "marked"
+// import Prism from "prismjs"
+// import loadLang from "prismjs/components/index.js"
+// import "prism-svelte"
 
-const loadLanguage = (name) => {
-    if (Prism.languages[name] !== undefined) {
-        return
-    }
-    loadLang(name)
-}
-const prismMarked = marked.use({
-    renderer: {
-        code(code, lang) {
-            loadLanguage(lang)
-            const hl = Prism.highlight(
-                code,
-                Prism.languages[lang],
-                lang
-            )
-            return `<pre class="language-${lang}"><code class="language-${lang}">${hl}</code></pre>`
-        }
-    }
-})
+// const loadLanguage = (name) => {
+//     if (Prism.languages[name] !== undefined) {
+//         return
+//     }
+//     loadLang(name)
+// }
+// const prismMarked = marked.use({
+//     renderer: {
+//         code(code, lang) {
+//             loadLanguage(lang)
+//             const hl = Prism.highlight(
+//                 code,
+//                 Prism.languages[lang],
+//                 lang
+//             )
+//             return `<pre class="language-${lang}"><code class="language-${lang}">${hl}</code></pre>`
+//         }
+//     }
+// })
 
-const root = path.resolve("docsite/example")
-const docsroot = path.resolve("docsite/docs")
-const exampleFiles = (await fs.readdir(
-    root,
-    { withFileTypes: true, recursive: true }
-)).filter(
-    ent => ent.isFile()
-).map(
-    ent => path.relative(
-        root,
-        path.resolve(ent.path, ent.name)
-    ).replaceAll(path.sep, "/")
-)
+// const root = path.resolve("docsite/example")
+// const docsroot = path.resolve("docsite/docs")
+// const exampleFiles = (await fs.readdir(
+//     root,
+//     { withFileTypes: true, recursive: true }
+// )).filter(
+//     ent => ent.isFile()
+// ).map(
+//     ent => path.relative(
+//         root,
+//         path.resolve(ent.path, ent.name)
+//     ).replaceAll(path.sep, "/")
+// )
 
-const componentList = exampleFiles.filter(
-    file => file.includes("/") === false && file.startsWith("--") === false
-)
-const exampleGroups = await Promise.all(
-    componentList.map(
-        async (file) => {
-            const id = path.basename(file, path.extname(file))
-            const name = id.replace(
-                /(^|\-)(\w)/g,
-                (_, _0, letter) => letter.toUpperCase()
-            )
-            const files = [
-                file,
-                ...exampleFiles.filter(
-                    exf => exf.startsWith(`${id}/`) === true
-                )
-            ]
-            const contents = await Promise.all(
-                files.map(
-                    filename => fs.readFile(
-                        path.resolve(root, filename),
-                        "utf8"
-                    )
-                )
-            )
-            const docs = await fs.readFile(
-                path.resolve(docsroot, `${id}.md`),
-                "utf8"
-            )
-            return {
-                id,
-                name,
-                path: path.resolve(root, file),
-                code: files.map(
-                    (filename, i) => [
-                        filename,
-                        Prism.highlight(
-                            contents[i],
-                            Prism.languages.svelte,
-                            "svelte"
-                        )
-                    ]
-                ),
-                docs: marked.parse(docs),
-            }
-        }
-    )
-)
+// const componentList = exampleFiles.filter(
+//     file => file.includes("/") === false && file.startsWith("--") === false
+// )
+// const exampleGroups = await Promise.all(
+//     componentList.map(
+//         async (file) => {
+//             const id = path.basename(file, path.extname(file))
+//             const name = id.replace(
+//                 /(^|\-)(\w)/g,
+//                 (_, _0, letter) => letter.toUpperCase()
+//             )
+//             const files = [
+//                 file,
+//                 ...exampleFiles.filter(
+//                     exf => exf.startsWith(`${id}/`) === true
+//                 )
+//             ]
+//             const contents = await Promise.all(
+//                 files.map(
+//                     filename => fs.readFile(
+//                         path.resolve(root, filename),
+//                         "utf8"
+//                     )
+//                 )
+//             )
+//             const docs = await fs.readFile(
+//                 path.resolve(docsroot, `${id}.md`),
+//                 "utf8"
+//             )
+//             return {
+//                 id,
+//                 name,
+//                 path: path.resolve(root, file),
+//                 code: files.map(
+//                     (filename, i) => [
+//                         filename,
+//                         Prism.highlight(
+//                             contents[i],
+//                             Prism.languages.svelte,
+//                             "svelte"
+//                         )
+//                     ]
+//                 ),
+//                 docs: marked.parse(docs),
+//             }
+//         }
+//     )
+// )
 
 export default {
     input: "docsite/index.html",
@@ -115,68 +115,68 @@ export default {
             targets: ["site/*.js", "site/*.js.map"],
             force: true,
         }),
-        {
-            resolveId(id) {
-                if (id === "$examples") {
-                    return id
-                }
-                if (id === "@axel669/zephyr") {
-                    return path.resolve("src/index.js")
-                }
-                return undefined
-            },
-            async load(file) {
-                if (file !== "$examples") {
-                    return
-                }
+        // {
+        //     resolveId(id) {
+        //         if (id === "$examples") {
+        //             return id
+        //         }
+        //         if (id === "@axel669/zephyr") {
+        //             return path.resolve("src/index.js")
+        //         }
+        //         return undefined
+        //     },
+        //     async load(file) {
+        //         if (file !== "$examples") {
+        //             return
+        //         }
 
-                const imports = exampleGroups.map(
-                    (ex) => `import ${ex.name}Example from ${JSON.stringify(ex.path)}`
-                )
-                const exports = exampleGroups.map(
-                    ex => {
-                        const id = JSON.stringify(ex.id)
-                        const name = JSON.stringify(ex.name)
-                        const code = JSON.stringify(ex.code)
-                        const docs = JSON.stringify(ex.docs)
-                        return `{
-                            id: ${id},
-                            name: ${name},
-                            code: ${code},
-                            docs: ${docs},
-                            component: ${ex.name}Example
-                        }`
-                    }
-                )
+        //         const imports = exampleGroups.map(
+        //             (ex) => `import ${ex.name}Example from ${JSON.stringify(ex.path)}`
+        //         )
+        //         const exports = exampleGroups.map(
+        //             ex => {
+        //                 const id = JSON.stringify(ex.id)
+        //                 const name = JSON.stringify(ex.name)
+        //                 const code = JSON.stringify(ex.code)
+        //                 const docs = JSON.stringify(ex.docs)
+        //                 return `{
+        //                     id: ${id},
+        //                     name: ${name},
+        //                     code: ${code},
+        //                     docs: ${docs},
+        //                     component: ${ex.name}Example
+        //                 }`
+        //             }
+        //         )
 
-                const output = [
-                    ...imports,
-                    "",
-                    "export default [",
-                    exports.join(",\n"),
-                    "]",
-                    ""
-                ].join("\n")
+        //         const output = [
+        //             ...imports,
+        //             "",
+        //             "export default [",
+        //             exports.join(",\n"),
+        //             "]",
+        //             ""
+        //         ].join("\n")
 
-                return output
-            }
-        },
-        {
-            async load(id) {
-                const filename = path.basename(id)
-                if (filename !== "readme.md") {
-                    return
-                }
-                const md = await fs.readFile(id, "utf8")
-                // extremely hacky replace to make it work nice on the site
-                // until i come back and make this more reliable
-                const html = prismMarked.parse(md).replace(
-                    "./docsite/docs/functions.md\"",
-                    "https://github.com/axel669/lib.zephyr/tree/live/docsite/docs/functions.md\" target=\"_blank\""
-                )
-                return `export default ${JSON.stringify(html)}`
-            }
-        },
+        //         return output
+        //     }
+        // },
+        // {
+        //     async load(id) {
+        //         const filename = path.basename(id)
+        //         if (filename !== "readme.md") {
+        //             return
+        //         }
+        //         const md = await fs.readFile(id, "utf8")
+        //         // extremely hacky replace to make it work nice on the site
+        //         // until i come back and make this more reliable
+        //         const html = prismMarked.parse(md).replace(
+        //             "./docsite/docs/functions.md\"",
+        //             "https://github.com/axel669/lib.zephyr/tree/live/docsite/docs/functions.md\" target=\"_blank\""
+        //         )
+        //         return `export default ${JSON.stringify(html)}`
+        //     }
+        // },
         html(),
         svelte({
             emitCss: false,
@@ -187,7 +187,7 @@ export default {
             }
         }),
         resolve({ browser: true }),
-        terser(),
-        copy("docsite/static"),
+        // terser(),
+        // copy("docsite/static"),
     ]
 }
