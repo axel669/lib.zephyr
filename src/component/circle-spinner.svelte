a7ae253d9569f1a48034f6907239d7feb4af2a87
axel669 <haryuu.hanekata@gmail.com>
2026-01-24 19:19:44 -0800

    [add] spinner components

---
+++ src/component/circle-spinner.svelte
@@ -0,0 +1,6 @@
+<script>
+    const { ws, ...rest } = $props()
+</script>
+
+<!-- svelte-ignore element_invalid_self_closing_tag -->
+<ws-circle-spinner data-ws={ws} {...rest} />
---
+++ src/component/hexagon-spinner.svelte
@@ -0,0 +1,6 @@
+<script>
+    const { ws, ...rest } = $props()
+</script>
+
+<!-- svelte-ignore element_invalid_self_closing_tag -->
+<ws-hexagon-spinner data-ws={ws} {...rest} />
