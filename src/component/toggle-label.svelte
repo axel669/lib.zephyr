248560eeffdcb5f7c3f2e91819ba31265dfab99b
axel669 <haryuu.hanekata@gmail.com>
2026-02-04 20:29:34 -0800

    [add] ToggleLabel component

---
+++ src/component/toggle-label.svelte
@@ -0,0 +1,11 @@
+<script>
+    const {
+        ws = "",
+        children,
+        ...rest
+    } = $props()
+</script>
+
+<label toggle data-ws={ws} {...rest}>
+    {@render children?.()}
+</label>
