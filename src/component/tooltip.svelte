fa10339cce78911ffdf1f39604b9330adb8de83d
axel669 <haryuu.hanekata@gmail.com>
2026-02-04 20:22:16 -0800

    [add] Tooltip component

---
+++ src/component/tooltip.svelte
@@ -0,0 +1,11 @@
+<script>
+    const {
+        ws,
+        children,
+        ...rest
+    } = $props()
+</script>
+
+<ws-tooltip data-ws={ws} {...rest}>
+    {@render children?.()}
+</ws-tooltip>
