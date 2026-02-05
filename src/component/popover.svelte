23e39faede2358d17413d81b779e1d501fa0ece0
axel669 <haryuu.hanekata@gmail.com>
2026-02-04 20:10:54 -0800

    [add] Popover component

---
+++ src/component/popover.svelte
@@ -0,0 +1,19 @@
+<script>
+    const {
+        content,
+        overlay,
+        ws = "",
+        ...rest
+    } = $props()
+
+    let popover = null
+    const show = () => popover.show()
+    const hide = () => popover.hide()
+</script>
+
+<ws-popover data-ws={ws} {...rest} bind:this={popover}>
+    {@render content(show)}
+    <ze-popover-overlay slot="popover">
+        {@render overlay(hide)}
+    </ze-popover-overlay>
+</ws-popover>
