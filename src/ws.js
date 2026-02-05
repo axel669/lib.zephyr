4b9e7e9f2cea1c57d0b17c237506f3072ecb1e29
axel669 <haryuu.hanekata@gmail.com>
2026-02-04 20:09:47 -0800

    [change] rename wsData to wsAttr

--- src/ws.js
+++ src/ws.js
@@ -3,7 +3,7 @@ import * as ws from "@axel669/windstorm"
 ws.macro("disp.grid")`disp: grid;`
 
 export { ws }
-export const wsData = (node, value) => {
+export const wsAttr = (node, value) => {
     const update = (value) => {
         node.setAttribute("data-ws", value)
     }
