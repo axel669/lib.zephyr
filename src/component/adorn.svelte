5ce6ca22c4b1f1c156166fabe0c448d6c58a231f
axel669 <haryuu.hanekata@gmail.com>
2026-02-04 20:10:14 -0800

    [add] adornments for control labels

---
+++ src/component/adorn.svelte
@@ -0,0 +1,9 @@
+<script>
+    const {
+        children
+    } = $props()
+</script>
+
+<ze-adorn data-ws="adorn;">
+    {@render children?.()}
+</ze-adorn>
--- src/component/control-label.svelte
+++ src/component/control-label.svelte
@@ -1,5 +1,14 @@
 <script>
-    const { ws, labelWS, label, children, ...rest } = $props()
+    const {
+        ws,
+        labelWS,
+        label,
+        children,
+        start,
+        end,
+        extra,
+        ...rest
+    } = $props()
 </script>
 
 <label control data-ws={ws ?? ""} {...rest}>
@@ -13,4 +22,20 @@
         </ws-text>
     {/if}
     {@render children()}
+
+    {#if start}
+        <ze-input-start data-ws="area: start; disp.grid;">
+            {@render start()}
+        </ze-input-start>
+    {/if}
+    {#if end}
+        <ze-input-end data-ws="area: end; disp.grid;">
+            {@render end()}
+        </ze-input-end>
+    {/if}
+    {#if extra}
+        <ze-input-extra data-ws="area: extra; disp.grid;">
+            {@render extra()}
+        </ze-input-extra>
+    {/if}
 </label>
