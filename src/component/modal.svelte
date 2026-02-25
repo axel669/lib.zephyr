587b09be06fb5e37ba2d11e021bc7de2b1c541b1
axel669 <haryuu.hanekata@gmail.com>
2026-02-24 22:03:06 -0800

    [change] animation durations for in and out on modal

--- src/component/modal.svelte
+++ src/component/modal.svelte
@@ -5,6 +5,9 @@
         ws = "",
         onclose,
         animTime,
+        inTime = animTime,
+        outTime = animTime,
+        persistent = false,
     } = $props()
 
     const vis = (node, opts) => {
@@ -22,7 +25,8 @@
 </script>
 
 {#if open === true}
-    <ws-modal open data-ws={ws} onclose={close} transition:vis={{ duration: animTime }}>
+    <ws-modal open data-ws={ws} onclose={close} {persistent}
+    in:vis={{ duration: inTime }} out:vis={{ duration: outTime }}>
         {@render children?.()}
     </ws-modal>
 {/if}
