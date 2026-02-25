4f751b6bac275158c7e33e1e6856599e18464482
axel669 <haryuu.hanekata@gmail.com>
2026-02-24 22:03:29 -0800

    [add] adornment to main exports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 export { ws, wsAttr } from "./ws.js"
 
+export { default as Adorn } from "./component/adorn.svelte"
 export { default as Avatar } from "./component/avatar.svelte"
 export { default as Badge } from "./component/badge.svelte"
 export { default as Button } from "./component/button.svelte"
