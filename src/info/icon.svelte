f5089692da4f38126db17b60120692ccaae6b064
axel669 <haryuu.hanekata@gmail.com>
2026-04-27 20:40:28 -0700

    [remove] old version of components that arent used anymore

--- src/info/icon.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import wsx from "../wsx.js"
-
-    const { name, children, ...rest } = $props()
-</script>
-
-<ws-icon data-icon={name} use:wsx={rest}>
-    {@render children?.()}
-</ws-icon>
